8a4e6bbeed83e5f83f4834f4362da495f17fb9ed
sangeeth <ssulochanaherath@gmail.com>
2024-11-24 19:54:31 +0530

    Exception package created.

--- src/main/java/lk/ijse/green_shadow_crop_system/service/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import lk.ijse.green_shadow_crop_system.dao.UserDao;
 import lk.ijse.green_shadow_crop_system.dto.impl.UserDTO;
 import lk.ijse.green_shadow_crop_system.entity.impl.UserEntity;
+import lk.ijse.green_shadow_crop_system.exception.DataPersistException;
 import lk.ijse.green_shadow_crop_system.service.UserService;
 import lk.ijse.green_shadow_crop_system.util.AppUtil;
 import lk.ijse.green_shadow_crop_system.util.Mapping;
@@ -23,7 +24,7 @@ public class UserServiceImpl implements UserService {
         userDTO.setUser_id(AppUtil.generateUserId());
         UserEntity saveUser = userDao.save(mapping.toUserEntity(userDTO));
         if (saveUser != null) {
-            throw new DataPersistenceException("Failed to save user");
+            throw new DataPersistException("Failed to save user");
         }
     }
 }
