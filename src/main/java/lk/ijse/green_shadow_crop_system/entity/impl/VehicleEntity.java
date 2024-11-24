0d13b605fc02a93d5181b0c6be4d7d4426d721a5
sangeeth <ssulochanaherath@gmail.com>
2024-11-24 18:40:11 +0530

    Few changes in Vehicle Entity.

--- src/main/java/lk/ijse/green_shadow_crop_system/entity/impl/VehicleEntity.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/entity/impl/VehicleEntity.java
@@ -1,4 +1,27 @@
 package lk.ijse.green_shadow_crop_system.entity.impl;
 
-public class VehicleEntity {
+import jakarta.persistence.*;
+import lk.ijse.green_shadow_crop_system.entity.Status;
+import lk.ijse.green_shadow_crop_system.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "vehicle")
+public class VehicleEntity implements SuperEntity {
+    @Id
+    private String vehicle_code;
+    private String licensePlateNumber;
+    private String vehicleCategory;
+    private String fuelType;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    private String remarks;
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private StaffEntity assigned_staff;
 }
