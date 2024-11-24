dbc3728762c54a1a517ccbc35a1bcec3aa8abe22
sangeeth <ssulochanaherath@gmail.com>
2024-11-24 20:03:57 +0530

    Few Changes in UserController.

--- src/main/java/lk/ijse/green_shadow_crop_system/controllers/UserController.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/controllers/UserController.java
@@ -1,10 +1,13 @@
 package lk.ijse.green_shadow_crop_system.controllers;
 
+import lk.ijse.green_shadow_crop_system.dto.impl.UserDTO;
+import lk.ijse.green_shadow_crop_system.exception.DataPersistException;
 import lk.ijse.green_shadow_crop_system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user")
@@ -12,4 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveUser(@RequestBody UserDTO userDTO) {
+        try {
+            userService.saveUser(userDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
