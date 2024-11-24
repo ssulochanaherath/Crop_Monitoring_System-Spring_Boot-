20ba3009a7fbe076b67a4d7c658a858eaeb1157d
sangeeth <ssulochanaherath@gmail.com>
2024-11-24 19:54:12 +0530

    Exception package created.

---
+++ src/main/java/lk/ijse/green_shadow_crop_system/exception/DataPersistException.java
@@ -0,0 +1,13 @@
+package lk.ijse.green_shadow_crop_system.exception;
+
+public class DataPersistException extends RuntimeException{
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+
+    }
+    public DataPersistException(String message, Throwable cause) {
+
+    }
+}
