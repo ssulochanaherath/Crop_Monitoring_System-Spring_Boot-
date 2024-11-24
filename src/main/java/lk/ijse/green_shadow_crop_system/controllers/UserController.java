6b22aabc5e3c89223e662d8d4b9c100bca844870
sangeeth <ssulochanaherath@gmail.com>
2024-11-24 19:22:01 +0530

    User Controller Created.

---
+++ src/main/java/lk/ijse/green_shadow_crop_system/controllers/UserController.java
@@ -0,0 +1,14 @@
+package lk.ijse.green_shadow_crop_system.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
