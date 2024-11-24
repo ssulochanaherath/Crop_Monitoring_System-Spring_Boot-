866731d33c41461dc4d692601cde6ff2e8dd3a89
sangeeth <ssulochanaherath@gmail.com>
2024-11-24 18:38:35 +0530

    Few changes in Crop Entity.

--- src/main/java/lk/ijse/green_shadow_crop_system/entity/impl/CropEntity.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/entity/impl/CropEntity.java
@@ -1,7 +1,6 @@
 package lk.ijse.green_shadow_crop_system.entity.impl;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lk.ijse.green_shadow_crop_system.entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,4 +12,15 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "crop")
 public class CropEntity implements SuperEntity {
+    @Id
+    private String crop_code;
+    private String common_name;
+    private String scientific_name;
+    @Column(columnDefinition = "LONGTEXT")
+    private String crop_image;
+    private String category;
+    private String season;
+    @ManyToOne
+    @JoinColumn(name = "field_code")
+    private FieldEntity field;
 }
