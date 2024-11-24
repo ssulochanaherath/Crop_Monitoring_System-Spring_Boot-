55e0bab7f9d88c518ac3e55c851c67fc6c5f5383
sangeeth <ssulochanaherath@gmail.com>
2024-11-24 18:19:05 +0530

    EquipmentType Enum Created.

--- src/main/java/lk/ijse/green_shadow_crop_system/entity/Designation.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/entity/Designation.java
@@ -2,5 +2,4 @@ package lk.ijse.green_shadow_crop_system.entity;
 
 public enum Designation {
     MANAGER,ADMINISTRATIVE,SCIENTIST,FIELD_WORKER
-
 }
--- src/main/java/lk/ijse/green_shadow_crop_system/entity/EquipmentType.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/entity/EquipmentType.java
@@ -2,4 +2,5 @@ package lk.ijse.green_shadow_crop_system.entity;
 
 public enum EquipmentType {
     ELECTRICAL,MECHANICAL
+
 }
