dd6dd7030e1b9db190b6585d0a6593d673d1440a
sangeeth <ssulochanaherath@gmail.com>
2024-11-25 19:27:47 +0530

    Few Changes.

--- src/main/java/lk/ijse/green_shadow_crop_system/controllers/UserController.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/user")
 @CrossOrigin
+
 public class UserController {
     @Autowired
     private UserService userService;
