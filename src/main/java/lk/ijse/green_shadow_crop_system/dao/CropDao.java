c337d68d54560e490d9083619f441cc13a20f17f
sangeeth <ssulochanaherath@gmail.com>
2024-11-28 22:11:51 +0530

    CropDao created.

--- src/main/java/lk/ijse/green_shadow_crop_system/dao/CropDao.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/dao/CropDao.java
@@ -4,10 +4,12 @@ import lk.ijse.green_shadow_crop_system.entity.impl.CropEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public class CropDao implements JpaRepository<CropEntity, String> {
+@Repository
+public interface CropDao extends JpaRepository<CropEntity, String> {
     @Query("SELECT c FROM CropEntity c WHERE c.common_name IN :crops")
     List<CropEntity> findByCropNameList(@Param("crops") List<String> crops);
 }
--- src/main/java/lk/ijse/green_shadow_crop_system/service/impl/CropServiceImpl.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/service/impl/CropServiceImpl.java
@@ -1,6 +1,5 @@
 package lk.ijse.green_shadow_crop_system.service.impl;
 
-import lk.ijse.green_shadow_crop_system.dao.CropDao;
 import lk.ijse.green_shadow_crop_system.dto.impl.CropDTO;
 import lk.ijse.green_shadow_crop_system.entity.impl.CropEntity;
 import lk.ijse.green_shadow_crop_system.service.CropService;
