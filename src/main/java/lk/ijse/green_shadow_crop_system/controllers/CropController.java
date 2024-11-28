9b012702a1c7100c34ec3f85deee9a94a4a3a434
sangeeth <ssulochanaherath@gmail.com>
2024-11-28 21:48:30 +0530

    Few Changes.

--- src/main/java/lk/ijse/green_shadow_crop_system/controllers/CropController.java
+++ src/main/java/lk/ijse/green_shadow_crop_system/controllers/CropController.java
@@ -1,4 +1,12 @@
 package lk.ijse.green_shadow_crop_system.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/crop")
+@CrossOrigin
 public class CropController {
+
 }
